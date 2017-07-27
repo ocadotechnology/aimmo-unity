f0dec7a1932eb2ecc6e6336b3ef1ab1608717be7
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2017-07-27 17:49:37 +0100

    Renamed 'Art' to 'Resources'.

--- Assets/Experimental/SpriteCreator.cs
+++ Assets/Experimental/SpriteCreator.cs
@@ -6,22 +6,16 @@ public class SpriteCreator : MonoBehaviour
 {
 	void Start () 
 	{
-		// Floor.
-		GameObject floor = GameObject.CreatePrimitive(PrimitiveType.Plane);
-		floor.transform.position = new Vector3(0.0f, -0.5f, 0.0f);
-		floor.transform.localScale = new Vector3(10.0f, 10.0f, 10.0f);
-		floor.GetComponent<Renderer>().material.color = Color.gray;
-
-		// Create a wall.
-		GameObject wall = new GameObject();
-		wall.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
-		Texture2D wallTexture = Resources.Load("../Art/brick-400x400-isometric-left.png") as Texture2D;
-		Sprite wallSprite = Sprite.Create(wallTexture, 
-			new Rect(0.0f, 0.0f, 400.0f, 400.0f),
+		// Create a grass.
+		GameObject grass = new GameObject("Grass");
+		grass.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+		Texture2D grassTexture = Resources.Load<Texture2D>("Grass-400x400-isometric-top");
+		Sprite grassSprite = Sprite.Create(grassTexture, 
+			new Rect(0.0f, 0.0f, 512.0f, 512.0f),
 			new Vector2(0.5f, 0.5f),
 			100.0f);
-		SpriteRenderer wallRenderer = wall.AddComponent<SpriteRenderer>();
-		wallRenderer.sprite = wallSprite;
+		SpriteRenderer grassRenderer = grass.AddComponent<SpriteRenderer>();
+		grassRenderer.sprite = grassSprite;
 
 	}
 }
