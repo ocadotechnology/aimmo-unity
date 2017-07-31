d007acbe669b9f71fee8f4f98b49fbffc11ba425
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2017-07-31 10:15:44 +0100

    Impemented FollowAvatar script with a smooth delay and tidied up file structure.

---
+++ Assets/Scripts/SceneView/FollowAvatar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowAvatar : MonoBehaviour 
+{
+	private GameObject target = null;
+
+	private const float xOffset = -5.0f;
+	private const float yOffset =  5.0f;
+	private const float zOffset = -5.0f;
+
+	private const float dampTime = 0.3f;
+	private Vector3 velocity = Vector3.zero;
+
+	void Update() 
+	{
+		if (target == null) 
+		{
+			target = GameObject.Find("player1");
+			return;
+		}
+
+		Vector3 destination= new Vector3(
+			target.transform.position.x + xOffset,
+			target.transform.position.y + yOffset,
+			target.transform.position.z + zOffset);
+	
+		transform.position = Vector3.SmoothDamp(
+			transform.position, 
+			destination, 
+			ref velocity, 
+			dampTime);
+	}
+}
