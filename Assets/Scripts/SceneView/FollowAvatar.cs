aaad667b67b2e4f62689fb1388d50f40dfad27ee
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2017-07-31 14:25:23 +0100

    Camera that follows avatar, notifies the cameras manager when it finishes moving.

--- Assets/Scripts/SceneView/FollowAvatar.cs
+++ Assets/Scripts/SceneView/FollowAvatar.cs
@@ -4,32 +4,53 @@ using UnityEngine;
 
 public class FollowAvatar : MonoBehaviour 
 {
+	/*
+	 * Script attached to the partent camera. It follows the main
+	 * player with a bit of a delay that makes it look more 
+	 * natural. Also, when it has stopped moving, it notifies 
+	 * the cameras manager to update the layers offset used to 
+	 * calculate the layer fo each game object. 
+	 */
+
 	private GameObject target = null;
 
 	private const float xOffset = -5.0f;
-	private const float yOffset =  5.0f;
+	private const float yOffset = 5.0f;
 	private const float zOffset = -5.0f;
 
 	private const float dampTime = 0.3f;
 	private Vector3 velocity = Vector3.zero;
+	private bool updatedLayers = false;
 
 	void Update() 
 	{
 		if (target == null) 
 		{
+			// TEMPORARY
 			target = GameObject.Find("player1");
 			return;
 		}
 
-		Vector3 destination= new Vector3(
+		Vector3 destination = new Vector3(
 			target.transform.position.x + xOffset,
 			target.transform.position.y + yOffset,
 			target.transform.position.z + zOffset);
-	
-		transform.position = Vector3.SmoothDamp(
-			transform.position, 
-			destination, 
-			ref velocity, 
-			dampTime);
+
+		if (destination != transform.position) 
+		{
+			transform.position = Vector3.SmoothDamp (
+				transform.position, 
+				destination, 
+				ref velocity, 
+				dampTime);
+			updatedLayers = false;
+		}
+		else if (!updatedLayers)
+		{
+			transform.GetComponent<CamerasManager>().UpdateCameraLayerOffset();
+			updatedLayers = true;
+		}
 	}
+
+
 }
