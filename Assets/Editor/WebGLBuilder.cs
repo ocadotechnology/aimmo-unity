3adcfcda788d24e1039b2698f25ddf0acf20fc00
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2017-08-08 16:59:48 +0100

    Added script to build from the command line.

---
+++ Assets/Editor/WebGLBuilder.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+
+/* To be used on the command line to build from the command line / script. The
+ * syntax is the following:
+ * 
+ * path/to/Unity -quit -batchmode -executeMethod WebGLBuilder.build
+ * 
+ * More details in https://docs.unity3d.com/Manual/CommandLineArguments.html
+ */
+
+class WebGLBuilder 
+{
+	static void build() 
+	{
+		string[] scenes = {"Assets/main.unity"};
+		BuildPipeline.BuildPlayer(
+			scenes, 
+			"WebGL-Dist", 
+			BuildTarget.WebGL, 
+			BuildOptions.Development);
+	}
+}
