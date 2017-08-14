d284cf976aa2721a5e38a4304259e15df818fb8e
Alexandru Dan <dan.alex97@yahoo.com>
2017-08-14 20:06:50 +0100

    Added a test to ensure that the player lurps for small values of time.

--- UnityProject/Assets/Editor/PlayerControllerTest.cs
+++ UnityProject/Assets/Editor/PlayerControllerTest.cs
@@ -47,20 +47,42 @@ namespace AIMMOUnityTest
 			}
 		}
 
-		[Test]
-		public void TestPlayerMovesToNextState() 
+		[TestCase(1.0f, 2.0f, 2.0f, 3.0f)]
+		[TestCase(0.0f, 0.0f, 10.0f, 10.0f)]
+		[TestCase(1.0f, 1.0f, 1.0f, 1.0f)]
+		[TestCase(0.0f, 0.0f, 0.0f, 10.0f)]
+		public void TestPlayerMovesToNextState(float x, float y, float x2, float y2) 
 		{
-			var wrapper = new PlayerControllerWrapper(1.0f, 2.0f);
-			wrapper.SetNextPosition (1.0f, 2.0f);
+			var wrapper = new PlayerControllerWrapper(x, y);
+			wrapper.SetNextPosition (x, y);
 			wrapper.playerController.Update();
 
-			wrapper.SetNextPosition(2.0f, 3.0f);
+			wrapper.SetNextPosition(x2, y2);
 			wrapper.PassTime (1.0f);
 
 			try { wrapper.playerController.Update();} 
-			catch (Exception e) {}
+			catch (Exception) {}
 
-			Assert.IsTrue(wrapper.GetPosition() == new Vector2(2.0f, 3.0f));
+			Assert.IsTrue(wrapper.GetPosition() == new Vector2(x2, y2));
 		}
+			
+		[TestCase(1.0f, 2.0f, 2.0f, 3.0f)]
+		[TestCase(0.0f, 0.0f, 10.0f, 10.0f)]
+		[TestCase(0.0f, 0.0f, 0.0f, 10.0f)]
+		public void TestPlayerLurps(float x, float y, float x2, float y2)
+		{
+			var wrapper = new PlayerControllerWrapper(x, y);
+			wrapper.SetNextPosition (x, y);
+			wrapper.playerController.Update();
+
+			wrapper.SetNextPosition(x2, y2);
+			wrapper.PassTime (0.05f);
+
+			try { wrapper.playerController.Update();} 
+			catch (Exception) {}
+
+			Assert.IsTrue(wrapper.GetPosition() != new Vector2(x2, y2));
+		}
+
 	}
 }
