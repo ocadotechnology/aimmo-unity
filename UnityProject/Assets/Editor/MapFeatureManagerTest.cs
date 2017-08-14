b7fa488bb5295fbb2faa5121179286597d897ef2
Alexandru Dan <dan.alex97@yahoo.com>
2017-08-14 15:24:12 +0100

    Added multiple tests for the internals of the MapFeatureManager. Also added a constructor in the mock class for different id generation.

--- UnityProject/Assets/Editor/MapFeatureManagerTest.cs
+++ UnityProject/Assets/Editor/MapFeatureManagerTest.cs
@@ -19,11 +19,16 @@ namespace AIMMOUnityTest
 		}";
 		private static MapFeatureData mfd 
 			= new MapFeatureData(JSON.Parse(mapFeatureDataJson));
-		private static string id = "test1";
 
 		public class ConcreteMapFeatureManager : MapFeatureManager
 		{
 			public bool drawCalled = false;
+			public string id;
+
+			public ConcreteMapFeatureManager(string id)
+			{
+				this.id = id;
+			}
 
 			override
 			public string MapFeatureId(string id)
@@ -40,15 +45,40 @@ namespace AIMMOUnityTest
 
 		[Test]
 		public void TestCreatingMapFeatureCallsDraw() {
-			var mapFeatureManager = new ConcreteMapFeatureManager ();
+			var mapFeatureManager = new ConcreteMapFeatureManager ("id1");
 
 			Assert.IsFalse (mapFeatureManager.drawCalled);
-			mapFeatureManager.Create (id, mfd);
+			mapFeatureManager.Create (mapFeatureManager.id, mfd);
 			Assert.IsTrue (mapFeatureManager.drawCalled);
 		}
 
 		[Test]
 		public void TestCreatingMapFeatureAddsGameObject() {
+			var mapFeatureManager = new ConcreteMapFeatureManager ("id2");
+
+			GameObject go = GameObject.Find(mapFeatureManager.id);
+			Assert.IsNull (go);
+
+			mapFeatureManager.Create (mapFeatureManager.id, mfd);
+
+			go = GameObject.Find(mapFeatureManager.id);
+			Assert.IsNotNull (go);
+		}
+			
+		[Test]
+		public void TestCreatingMapFeatureAddsComponents() {
+			var mapFeatureManager = new ConcreteMapFeatureManager ("id3");
+		
+			mapFeatureManager.Create (mapFeatureManager.id, mfd);
+			GameObject go = GameObject.Find(mapFeatureManager.id);
+
+			Assert.IsNotNull (go.GetComponent<IsometricPosition>());
+			Assert.AreEqual (go.transform.rotation, Quaternion.Euler(45.0f, 45.0f, 0.0f));
+
+			IsometricPosition component = go.GetComponent<IsometricPosition> ();
+			Vector2 position = new Vector2(1.0f, 2.0f);
+
+			Assert.AreEqual (component.Vector(), position);
 		}
 	}
 }
