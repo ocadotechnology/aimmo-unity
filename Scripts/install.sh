814a2e1bab618427216d7786a11735218b94a5b0
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2017-08-15 14:08:50 +0100

    Changed travis configuration and build/install scripts.

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,21 +1,26 @@
 #! /bin/sh
 
-# Download Unity3D installer into the container
-#  The below link will need to change depending on the version, this one is for 5.5.1
-#  Refer to https://unity3d.com/get-unity/download/archive and find the link pointed to by Mac "Unity Editor"
-echo 'Downloading Unity 5.5.1 pkg:'
-curl --retry 5 -o Unity.pkg http://netstorage.unity3d.com/unity/88d00a7498cd/MacEditorInstaller/Unity-5.5.1f1.pkg
-if [ $? -ne 0 ]; then { echo "Download failed"; exit $?; } fi
+if [ ! -d "$HOME/UnityDownloads" ]; then
+	mkdir $HOME/UnityDownloads
+fi
 
-# In Unity 5 they split up build platform support into modules which are installed separately
-# By default, only Mac OSX support is included in the original editor package; Windows, Linux, iOS, Android, and others are separate
-# In this example we download Windows support. Refer to http://unity.grimdork.net/ to see what form the URLs should take
-echo 'Downloading Unity 5.5.1 Windows Build Support pkg:'
-curl --retry 5 -o Unity_win.pkg http://netstorage.unity3d.com/unity/88d00a7498cd/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-5.5.1f1.pkg
-if [ $? -ne 0 ]; then { echo "Download failed"; exit $?; } fi
+if [ ! -f "$HOME/UnityDownloads/Unity.pkg" ]; then
+	echo 'Downloading Unity 5.5.0 pkg:'
+	curl --retry 5 -m 600 -o $HOME/UnityDowloads/Unity.pkg http://download.unity3d.com/download_unity/38b4efef76f0/MacEditorInstaller/Unity.pkg
+	if [ $? -ne 0 ]; then { echo "Download failed"; exit $?; } fi
+else
+	echo 'Unity package found in cache.'
+fi
 
-# Run installer(s)
+if [ ! -f "$HOME/UnityDownloads/Unity.pkg" ]; then
+	echo 'Downloading Unity 5.5.0 WebGL Build Support pkg:'
+	curl --retry 5 -o $HOME/UnityDowloads/Unity_webgl.pkg http://download.unity3d.com/download_unity/38b4efef76f0/MacEditorTargetInstaller/UnitySetup-WebGL-Support-for-Editor-5.5.0f3.pkg
+	if [ $? -ne 0 ]; then { echo "Download failed"; exit $?; } fi
+else
+	echo 'Unity WebGL support package found in cache.'
+fi
+
 echo 'Installing Unity.pkg'
-sudo installer -dumplog -package Unity.pkg -target /
-echo 'Installing Unity_win.pkg'
-sudo installer -dumplog -package Unity_win.pkg -target /
+sudo installer -dumplog -package $HOME/UnityDowloads/Unity.pkg -target /
+echo 'Installing Unity_webgl.pkg'
+sudo installer -dumplog -package $HOME/UnityDowloads/Unity_webgl.pkg -target /
