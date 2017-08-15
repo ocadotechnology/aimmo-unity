5c0110adf0b40b49df63041dba545947f4336c04
Alexandru Dan <dan.alex97@yahoo.com>
2017-08-15 17:52:21 +0100

    Added a WebGL build file. This build works has been tested from Ubuntu.

---
+++ UnityProject/Assets/Editor/WebGLBuilder.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+
+/*
+ Run from unity installation folder:
+  $  ./Unity -batchmode -projectPath <projectPath> -logfile -executeMethod WebGLBuilder.build
+ e.g:
+  $ ./Unity -batchmode -projectPath ~/aimmo-unity/UnityProject/ -logfile -executeMethod WebGLBuilder.build
+*/
+
+class WebGLBuilder {
+	static void build() {
+		string[] scenes = {
+			"Assets/Scenes/Main.unity"
+		};
+
+		/*
+			Asset Bundles gives an error for the momment.
+		*/
+		BuildPipeline.BuildPlayer(scenes, "WebGL-Dist", BuildTarget.WebGL,
+			BuildOptions.Development |
+			BuildOptions.AllowDebugging |
+			BuildOptions.UncompressedAssetBundle);
+	}
+}
