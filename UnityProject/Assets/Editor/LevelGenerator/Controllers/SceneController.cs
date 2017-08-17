88b46129d8108f5a477ec2ee4604ecb8d53f1b26
Alexandru Dan <dan.alex97@yahoo.com>
2017-08-17 15:43:08 +0100

    Moved the generated window around and added a controller. We now can see all levels.

---
+++ UnityProject/Assets/Editor/LevelGenerator/Controllers/SceneController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+using System.Collections;
+
+public class SceneController
+{
+	private static SceneController controllerInstace = new SceneController();
+
+	public static void CreateLevel (string sceneName)
+	{
+		if (!controllerInstace.IsScenePresent (sceneName)) 
+		{
+			Debug.Log ("Creating new scene " + sceneName);	
+		}
+	}
+
+	private bool IsScenePresent(string sceneName) 
+	{
+		foreach (EditorBuildSettingsScene S in EditorBuildSettings.scenes) 
+		{
+			string name = S.path.Substring(S.path.LastIndexOf('/') + 1);
+			name = name.Substring(0, name.Length - 6);
+			if (name == sceneName) 
+			{
+				return true;
+			}
+		}	
+		return false;
+	}
+}
+
---
+++ UnityProject/Assets/Editor/LevelGenerator/GUI/GeneratorWindow.cs
@@ -0,0 +1,47 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class GeneratorWindow : EditorWindow
+{
+	private static GeneratorWindow windowInstace;
+	private string sceneName = "Name";
+
+	[MenuItem("Window/Create Level")]
+	public static void OpenWindow()
+	{
+		windowInstace = GetWindow<GeneratorWindow>(false, "Unity Level Generator");
+
+		windowInstace.Focus ();
+		windowInstace.Show ();
+	}
+
+	private void CreateLevel()
+	{
+		SceneController.CreateLevel (sceneName);
+	}
+
+	private void CreateButton() 
+	{
+		GUILayout.Label("Select an object in the hierarchy view");
+		sceneName = EditorGUILayout.TextField("Scene Name: ", sceneName);
+
+		if (GUILayout.Button (new GUIContent ("Create Level", ""))) 
+		{
+			CreateLevel ();
+		}
+	}
+
+	private void CloseButton() 
+	{
+		if (GUILayout.Button (new GUIContent ("Close", ""))) 
+		{
+			windowInstace.Close ();
+		}
+	}
+
+	public void OnGUI()
+	{
+		CreateButton ();
+		CloseButton ();
+	}
+}
