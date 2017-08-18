25a12d4810140c8d5cdb265493aed8eeb549bd4b
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2017-08-18 16:32:55 +0100

    Building in Travis.

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,10 +1,5 @@
 #! /bin/sh
 
-# NOTE the command args below make the assumption that your Unity project folder is
-#  a subdirectory of the repo root directory, e.g. for this repo "unity-ci-test" 
-#  the project folder is "UnityProject". If this is not true then adjust the 
-#  -projectPath argument to point to the right location.
-
 ## Run the editor unit tests
 echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME}"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
@@ -23,3 +18,23 @@ cat $(pwd)/test.xml
 
 # exit if tests failed
 if [ $rc0 -ne 0 ]; then { echo "Failed unit tests"; exit $rc0; } fi
+
+## This will build to webGL and put the binaries in WebGL-Dist.
+echo "Attempting build of ${UNITYCI_PROJECT_NAME} for WebGL"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+	-batchmode \
+	-nographics \
+	-silent-crashes \
+	-logFile $(pwd)/unity.log \
+	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
+	-executeMethod WebGLBuild.build \
+	-quit
+
+rc1=$?
+echo "Build logs (Windows)"
+cat $(pwd)/unity.log
+
+# exit if build failed
+if [ $rc1 -ne 0 ]; then { echo "Failed build"; exit $rc0; } fi
+
+exit $(($rc0|$rc1))
