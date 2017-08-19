d993985323cd586e4f9e2d6c59896380823d3c29
Alexandru Dan <dan.alex97@yahoo.com>
2017-08-20 00:31:23 +0100

    Changed the GUI so it uses the provided builders. Now we have the generated objects.

--- UnityProject/Assets/Editor/LevelGenerator/GUI/GeneratorWindow.cs
+++ UnityProject/Assets/Editor/LevelGenerator/GUI/GeneratorWindow.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEditor;
 using UnityEngine;
 using GeneratorNS;
+using System;
 
 public class GeneratorWindow : EditorWindow
 {
 	private static GeneratorWindow windowInstace;
 	private string sceneName = "Name";
+
+	private class GenData
+	{
+		public string name = "name";
+		public string x = "0";
+		public string y = "0";
+	}
 
 	[MenuItem("Window/Create Level")]
 	public static void OpenWindow()
@@ -39,6 +47,34 @@ public class GeneratorWindow : EditorWindow
 		GUILayout.Label("Current selected object: " + go.name);
 	}
 
+	private GenData obstacleData = new GenData();
+	private GenData healthData = new GenData();
+
+	private void GeneratorMenu()
+	{
+		//EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+		GUILayout.Label("Generator interface.");
+
+		GeneratorGUI (typeof(ObstacleGenerator), "Gen obstacle", obstacleData);
+		GeneratorGUI (typeof(HealthPointGenerator), "Gen health point", healthData);
+
+	}
+
+	private void GeneratorGUI(Type generatorType, string buttonName, GenData data)
+	{
+		SpriteGeneratorBuilder builder = new SpriteGeneratorBuilder (typeof(ObstacleGenerator));
+
+		GUILayout.Label ("Name:"); data.name = GUILayout.TextField (data.name, 10);
+		GUILayout.Label ("X:"); data.x = GUILayout.TextField (data.x, 5);
+		GUILayout.Label ("Y:"); data.y = GUILayout.TextField (data.y, 5);
+
+		if (GUILayout.Button (new GUIContent (buttonName))) 
+		{
+			builder = builder.ByCoord (float.Parse(data.x), float.Parse(data.y));
+			builder.Build ().GenerateObject (data.name);
+		}
+	}
+
 	private void SaveButton()
 	{
 	}
@@ -52,22 +88,17 @@ public class GeneratorWindow : EditorWindow
 			LevelButton (level);
 		}
 
+		SaveButton ();
+
+		GeneratorMenu ();
+
 		if (ObjectController.SelectedGameObject ()) 
 		{
+			//EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
 			ObjectMenu ();
 		}
 
-		if (GUILayout.Button (new GUIContent ("Gen obstacle"))) 
-		{
-			new SpriteGeneratorBuilder(typeof(ObstacleGenerator)).Build().GenerateObject("id1");
-		}
-			
-		if (GUILayout.Button (new GUIContent ("Gen health point"))) 
-		{			
-			new SpriteGeneratorBuilder(typeof(HealthPointGenerator)).Build().GenerateObject("hp1");
-		}
-
-		SaveButton ();
+		//EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
 
 		CloseButton ();
 	}
