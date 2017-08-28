cceba8ac8c94883e3efa10e298de6a63d43b1dd0
Alexandru Dan <dan.alex97@yahoo.com>
2017-08-28 18:02:51 +0100

    Added a function to export a level to a file.

--- UnityProject/Assets/Editor/LevelGenerator/Controllers/ExportController.cs
+++ UnityProject/Assets/Editor/LevelGenerator/Controllers/ExportController.cs
@@ -5,12 +5,16 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using Serializers;
+using UnityEngine.SceneManagement;
 
 public class ExportController
 {
 	/** 
 	 * A class used for exprting the contents of a level. 
+	 * 
+	 * The code does not work on Windows for the moment.
 	 */
+
 	public static string GetExportFromSerializer(ISerializer serializer)
 	{
 		return serializer.Serialize();
@@ -18,6 +22,14 @@ public class ExportController
 
 	public static void ExportFile(string json)
 	{
+		string sceneName = SceneManager.GetActiveScene ().name + ".json";
+		string exportDirectory = Directory.GetCurrentDirectory () + "/Exports/";
+
+		Directory.CreateDirectory(exportDirectory);
+		string exportLocation = exportDirectory + sceneName;
+
+		Debug.Log ("Exporting json to:" + exportLocation);
+		File.WriteAllText (exportLocation, json); 
 	}
 }
 
