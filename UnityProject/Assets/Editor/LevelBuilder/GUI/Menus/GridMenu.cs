fd1ccdd6cd419579e1e7d56b3b9324d871c08628
Olaf Szmidt <szmidtolaf@gmail.com>
2017-09-14 16:52:13 +0100

    Fixed #40 and a few extra comments
    Unnecessary comments removed
    
    Endline added again
    
    Debug message changed accordingly

--- UnityProject/Assets/Editor/LevelBuilder/GUI/Menus/GridMenu.cs
+++ UnityProject/Assets/Editor/LevelBuilder/GUI/Menus/GridMenu.cs
@@ -8,43 +8,39 @@ using UnityEngine;
  * Allows the user to choose when to display the grid and how big it should be.
  */
 
-public class GridMenu : IMenu
-{
+public class GridMenu: IMenu {
 	private bool showGrid = true;
 
 	private string width = "10";
 	private string height = "10";
 
-	public void Display()
-	{
-		EditorGUILayout.LabelField ("", GUI.skin.horizontalSlider);
+	public void Display() {
+		EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
 
+		// Enclosing in a BeginHorizontal() to indicate when to end and
+		// move to the next horizontal line.
 		GUILayout.BeginHorizontal();
 
 		// Grid toggle.
 		showGrid = EditorGUILayout.Toggle("Grid", showGrid);
 
-		// Width and height labels and text fields.
-		GUILayout.Label("Width: ");
-		width = GUILayout.TextField(width, GUILayout.MaxWidth(30));
-		GUILayout.Label("Height: ");
-		height = GUILayout.TextField(height, GUILayout.MaxWidth(30));
+		// Move to next line in the GUI.
+		GUILayout.EndHorizontal();
+		GUILayout.BeginHorizontal();
 
+		width = GUIUtils.TextField("Width: ", width);
+		height = GUIUtils.TextField("Height: ", height);
+
+		// Move to next line in the GUI.
 		GUILayout.EndHorizontal();
 
-		if (showGrid) 
-			try 
-			{
-				GridVisualiser.BuildGrid (Convert.ToInt32 (width), Convert.ToInt32 (height));
-			}
-			catch (FormatException exception) 
-			{
-				Debug.Log (
-					"Grid width/height format exception." +
-					"Please make sure you write an integer.");
-			}
-		else
-			GridVisualiser.DestroyGrid();
+		if (showGrid) try {
+			GridVisualiser.BuildGrid(Convert.ToInt32(width), Convert.ToInt32(height));
+		}
+		catch(FormatException exception) {
+			Debug.Log("Grid width/height format exception. Make sure you input a valid number (integer).");
+		}
+		else GridVisualiser.DestroyGrid();
 	}
 
 }
---
+++ UnityProject/Assets/Editor/LevelBuilder/Util/GUIUtils.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GUIUtils
+{
+	// A utility method that takes creates EditorGUILayout.TextField
+	// with no space between the label and the textfield.
+	public static string TextField(string label, string text)
+	{
+		var textDimensions = GUI.skin.label.CalcSize (new GUIContent (label));
+		EditorGUIUtility.labelWidth = textDimensions.x;
+
+		return EditorGUILayout.TextField (label, text);
+	}
+}
+	
