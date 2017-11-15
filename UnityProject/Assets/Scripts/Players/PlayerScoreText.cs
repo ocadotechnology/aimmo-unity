4813095c256c24ad58cca5ecfc7ba7e733d7127a
Niket Shah <masterniket@gmail.com>
2017-11-15 10:37:29 +0000

    namespace Players code

--- UnityProject/Assets/Scripts/Players/PlayerScoreText.cs
+++ UnityProject/Assets/Scripts/Players/PlayerScoreText.cs
@@ -1,45 +1,46 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-/* Creates the text on top of each avatar with the score.
- */
-
-public class PlayerScoreText : MonoBehaviour
+namespace Players
 {
-	// Text positioning.
-	private const float ScaleX = 0.1f;
-	private const float ScaleY = 0.1f;
-	private const float ScaleZ = 0.1f;
 
-	private const float OffsetX = 0.0f;
-	private const float OffsetY = 1.5f;
-	private const float OffsetZ = 0.0f;
+    /* Creates the text on top of each avatar with the score.
+     */
 
-	private const int fontSize = 80;
+    public class PlayerScoreText : MonoBehaviour
+    {
+        // Text positioning.
+        private const float ScaleX = 0.1f;
+        private const float ScaleY = 0.1f;
+        private const float ScaleZ = 0.1f;
 
-	private GameObject text;
+        private const float OffsetX = 0.0f;
+        private const float OffsetY = 1.5f;
+        private const float OffsetZ = 0.0f;
 
-	void Awake()
-	{
-		text = new GameObject("Text");
-		Vector3 textPosition = transform.position;
-		textPosition.x += OffsetX;
-		textPosition.y += OffsetY;
-		textPosition.z += OffsetZ;
-		text.transform.position = textPosition;
-		text.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
-		text.transform.localScale = new Vector3(ScaleX, ScaleY, ScaleZ);
+        private const int fontSize = 80;
 
-		text.AddComponent<TextMesh>();
-		text.GetComponent<TextMesh>().fontSize = fontSize;
+        private GameObject text;
 
-		text.transform.parent = transform;
-	}
+        void Awake()
+        {
+            text = new GameObject("Text");
+            Vector3 textPosition = transform.position;
+            textPosition.x += OffsetX;
+            textPosition.y += OffsetY;
+            textPosition.z += OffsetZ;
+            text.transform.position = textPosition;
+            text.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+            text.transform.localScale = new Vector3(ScaleX, ScaleY, ScaleZ);
 
-	public void SetScore(string score)
-	{
-		text.GetComponent<TextMesh>().text = score;
-	}
+            text.AddComponent<TextMesh>();
+            text.GetComponent<TextMesh>().fontSize = fontSize;
+
+            text.transform.parent = transform;
+        }
+
+        public void SetScore(string score)
+        {
+            text.GetComponent<TextMesh>().text = score;
+        }
+    }
 }
