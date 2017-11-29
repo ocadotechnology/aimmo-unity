6c24dcb802c893923077fd0f9f8ce8087b3dd556
Olaf Szmidt <szmidtolaf@gmail.com>
2017-11-29 18:45:07 +0000

    Obstacle Level Builder (#85)
    * Initial Obstacle Level Builder implementation
    
    * Remove Level files from brachiosaurus branch
    
    * Add Obstacle tag to prefabs
    
    * Change brahiosaurus model to suit size better
    
    * Fix a object picker listener bug

---
+++ UnityProject/Assets/Editor/LevelBuilder/GUI/Menus/ObstacleMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ObstacleMenu : IMenu
+{
+    private GUIContent obstaclePickerButtonTitle = new GUIContent("View Obstacles");
+
+    public void Display()
+    {
+        DisplayTitle();
+        DisplayObstaclesPickerButton();
+    }
+
+    private void DisplayTitle()
+    {
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Obstacles", EditorStyles.largeLabel);
+        EditorGUILayout.EndHorizontal();
+    }
+        
+    private void DisplayObstaclesPickerButton()
+    {
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button(obstaclePickerButtonTitle))
+        {
+
+            EditorGUIUtility.ShowObjectPicker<GameObject>(null, false, "obstacle_", GUIUtility.GetControlID(obstaclePickerButtonTitle, FocusType.Passive));
+        }
+        EditorGUILayout.EndHorizontal();
+    }
+}
+
