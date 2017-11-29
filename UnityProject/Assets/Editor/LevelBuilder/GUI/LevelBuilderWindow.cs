4fa3ea6fe0ddcb5aa494dff5a58b34d258066aef
Niket Shah <masterniket@gmail.com>
2017-11-29 10:28:54 +0000

    Format the LevelBuilderWindow.cs

--- UnityProject/Assets/Editor/LevelBuilder/GUI/LevelBuilderWindow.cs
+++ UnityProject/Assets/Editor/LevelBuilder/GUI/LevelBuilderWindow.cs
@@ -5,102 +5,102 @@ using UnityEditor.SceneManagement;
 using System.Linq;
 using System.Collections.Generic;
 
-/* The UI Window from Level Generator/Create Level. 
- * 
- * The Window is split in Menu components. Each Menu component is a class 
- * that drows on the currently selected UI using the GUILayout and GUIEditorLayout 
+/* The UI Window from Level Generator/Create Level.
+ *
+ * The Window is split in Menu components. Each Menu component is a class
+ * that drows on the currently selected UI using the GUILayout and GUIEditorLayout
  * static methods.
- * 
+ *
  * OnGUI looks for events that are applied on the surface of the UI each time.
- * We use the Singleton pattern in this situation as there should be only one 
+ * We use the Singleton pattern in this situation as there should be only one
  * object of this type: i.e. the open window.
- * 
- */ 
+ *
+ */
 
 public class LevelBuilderWindow : EditorWindow
 {
-	private static LevelBuilderWindow windowInstance = null;
-	private int optionChosenByUser = 0;
+    private static LevelBuilderWindow windowInstance = null;
+    private int optionChosenByUser = 0;
 
-	// Main menus.
-	private IMenu[] menus = {
+    // Main menus.
+    private IMenu[] menus = {
         new TerrainMenu(),
         new PickupMenu(),
-	};
+    };
 
-	private static LevelBuilderWindow GetWindow()
-	{
-		return windowInstance;
-	}
+    private static LevelBuilderWindow GetWindow()
+    {
+        return windowInstance;
+    }
 
-	// 'Open' drop-down option.
-	[MenuItem("Level Builder/Open")]
-	public static void Open()	
-	{
-		windowInstance = GetWindow<LevelBuilderWindow>(false, "Level Builder");
+    // 'Open' drop-down option.
+    [MenuItem("Level Builder/Open")]
+    public static void Open()
+    {
+        windowInstance = GetWindow<LevelBuilderWindow>(false, "Level Builder");
 
-		windowInstance.Show();
-		windowInstance.Focus();
+        windowInstance.Show();
+        windowInstance.Focus();
 
-		SetUpScene();
-	}
+        SetUpScene();
+    }
 
-	// 'Help' drop-down option. Go to github.
-	[MenuItem("Level Builder/Help")]
-	public static void Help()
-	{
-		Application.OpenURL("https://github.com/ocadotechnology/aimmo-unity/");
-	}
+    // 'Help' drop-down option. Go to github.
+    [MenuItem("Level Builder/Help")]
+    public static void Help()
+    {
+        Application.OpenURL("https://github.com/ocadotechnology/aimmo-unity/");
+    }
 
-	// Any intialisation required for the scene when building a level goes here.
-	private static void SetUpScene()
-	{
-		Camera camera = Camera.main;
+    // Any intialisation required for the scene when building a level goes here.
+    private static void SetUpScene()
+    {
+        Camera camera = Camera.main;
 
-		camera.transform.position = new Vector3(0.0f, 0.0f, -100.0f);
-		camera.transform.rotation = Quaternion.Euler(Vector3.zero);
-		camera.orthographic = true;
-		camera.orthographicSize = 5.0f;
-		camera.farClipPlane = 1000.0f;
-		camera.nearClipPlane = 0.3f;
-	}
+        camera.transform.position = new Vector3(0.0f, 0.0f, -100.0f);
+        camera.transform.rotation = Quaternion.Euler(Vector3.zero);
+        camera.orthographic = true;
+        camera.orthographicSize = 5.0f;
+        camera.farClipPlane = 1000.0f;
+        camera.nearClipPlane = 0.3f;
+    }
 
     public void OnGUI()
-	{
+    {
         minSize = new Vector2(225, 0);
 
-		// Level buttons.
-		GUILayout.Label("Select a level below to work on:");
+        // Level buttons.
+        GUILayout.Label("Select a level below to work on:");
 
-		// Get all levels currently in the game.
-		IList<string> levelList = AssetFetcher.GetLevels();
+        // Get all levels currently in the game.
+        IList<string> levelList = AssetFetcher.GetLevels();
 
-		// Popup() expects a string[] array, so convert it.
-		string[] levels = levelList.ToArray<string>();
+        // Popup() expects a string[] array, so convert it.
+        string[] levels = levelList.ToArray<string>();
 
-		// optionChosenByUser initialised to 0, so the dropdown will be blank. 
-		// After an option is chosen for the first time, we update this.
+        // optionChosenByUser initialised to 0, so the dropdown will be blank.
+        // After an option is chosen for the first time, we update this.
 
-		optionChosenByUser--;
+        optionChosenByUser--;
 
-		optionChosenByUser = EditorGUILayout.Popup(optionChosenByUser, levels);
+        optionChosenByUser = EditorGUILayout.Popup(optionChosenByUser, levels);
 
-		// Index is returned and levels start from 1 so incrementing it.
-		optionChosenByUser++;
+        // Index is returned and levels start from 1 so incrementing it.
+        optionChosenByUser++;
 
-		if (GUILayout.Button (new GUIContent ("Work!")))
-			AssetFetcher.WorkOnLevel ("Level" + optionChosenByUser);
-		
-		// Only let the level designer operate if it's a level.
-		// ie. If the scene is called "Main" then it is not a level.
-		// See the file hierarchy to understand.
-		if (SceneManager.GetActiveScene().name.Equals("Main"))
-			return;
+        if (GUILayout.Button(new GUIContent("Work!")))
+            AssetFetcher.WorkOnLevel("Level" + optionChosenByUser);
 
-		if (GUILayout.Button (new GUIContent ("Create a new level.")))
+        // Only let the level designer operate if it's a level.
+        // ie. If the scene is called "Main" then it is not a level.
+        // See the file hierarchy to understand.
+        if (SceneManager.GetActiveScene().name.Equals("Main"))
+            return;
+
+        if (GUILayout.Button(new GUIContent("Create a new level.")))
         {
 
-            // We have a bug where Unity doesn't recognise our dynamically 
+            // We have a bug where Unity doesn't recognise our dynamically
             // added objects through the level builder so we need to force
             // the change to be saved.
             EditorSceneManager.MarkAllScenesDirty();
@@ -111,8 +111,8 @@ public class LevelBuilderWindow : EditorWindow
             // Unload the scenes so we can create a new one.
             SceneHandler.unloadScenes();
 
-            // Calling countScenes() inside SceneHandler to find the new 
-            // filename to save our scene with. 
+            // Calling countScenes() inside SceneHandler to find the new
+            // filename to save our scene with.
             int currentNoOfLevels = SceneHandler.countScenes();
             currentNoOfLevels++;
             SetupCamera();
@@ -133,7 +133,7 @@ public class LevelBuilderWindow : EditorWindow
             menu.Display();
         }
         EditorGUILayout.EndScrollView();
-	}
+    }
 
     private static void SetupCamera()
     {
