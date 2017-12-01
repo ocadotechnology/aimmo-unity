8193e19871cefa94ea236275ad404eefc3ccee27
Maria Mafalda Fernando <maria.fernando@ocado.com>
2017-12-01 16:03:33 +0000

    Copied movement code to PlayerController script

--- UnityProject/Assets/Scripts/Players/PlayerController.cs
+++ UnityProject/Assets/Scripts/Players/PlayerController.cs
@@ -11,8 +11,12 @@ namespace Players
     public class PlayerController : MonoBehaviour
     {
         // General movement variables.
-        private const float speed = 5.0f;
+        private const float speed = 2.0f;
+        private Vector3 velocity = new Vector3(0, 0, 0);
         private const float moveInterval = 0.5f;
+
+        private Animator anim;
+        private CharacterController controller;
 
         private float startTime;
 
@@ -20,17 +24,25 @@ namespace Players
         private Vector2 currPosition;
         private Vector2 nextPosition;
 
+        // Temporary variables
+        public string direction = "north";
+        public int playerIsMoving = 0;
+                
+
         // Initialisation.
         public void Awake()
         {
             startTime = Time.time;
             currPosition = transform.position;
+
+            controller = GetComponent<CharacterController>();
+            anim = GetComponentInChildren<Animator>();
         }
 
         // Move the player to next position.
         public void Update()
         {
-            // TODO: Rotation transition. Maybe an animation?
+            /*// TODO: Rotation transition. Maybe an animation?
             float step = (Time.time - startTime) * speed;
 
             if (step < moveInterval)
@@ -57,7 +69,42 @@ namespace Players
                 // Update healthbar.
                 int hp = nextState.health;
                 transform.GetComponent<PlayerHealthBar>().SetHealthPoints(hp);
+            }*/
+
+            
+            if (playerIsMoving == 1){
+                anim.SetInteger ("AnimParam", 1);
+
+                // positive x axis
+                if (direction == "north"){
+                    gameObject.transform.eulerAngles = new Vector3(0, 90, 0);
+                    velocity = new Vector3(speed, 0, 0);
+                }
+
+                // negative x axix
+                else if (direction == "south"){
+                    gameObject.transform.eulerAngles = new Vector3(0, -90, 0);
+                    velocity = new Vector3(-speed, 0, 0);
+                }
+
+                // positive z axix
+                else if (direction == "east"){
+                    gameObject.transform.eulerAngles = new Vector3(0, 0, 0);
+                    velocity = new Vector3(0, 0, speed);
+                }
+
+                // negative z axix
+                else if (direction == "west"){
+                    gameObject.transform.eulerAngles = new Vector3(0, 180, 0);
+                    velocity = new Vector3(0, 0, -speed);
+                }
             }
+            else{
+                velocity = new Vector3(0, 0, 0);
+                anim.SetInteger ("AnimParam", 0);
+            }
+
+            transform.position += velocity * Time.deltaTime;
         }
 
         // Set next destination.
