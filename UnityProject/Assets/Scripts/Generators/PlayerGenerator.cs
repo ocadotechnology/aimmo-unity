9b7b4a6efe2e0e7ff0c1075f4219029317cfbaa5
Olaf Szmidt <szmidtolaf@gmail.com>
2017-12-13 16:28:38 +0000

    Change the Manager and Generator for player to not reload every time
    a new player is created.

--- UnityProject/Assets/Scripts/Generators/PlayerGenerator.cs
+++ UnityProject/Assets/Scripts/Generators/PlayerGenerator.cs
@@ -1,12 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 using Players;
 
 public class PlayerGenerator : MonoBehaviour
 {
+    static Material[] materials = Resources.LoadAll<Material>("Materials/Players");
+    static GameObject deePrefab = Resources.Load<GameObject>("Prefabs/Players/player_dee");
+
     public static GameObject GeneratePlayer(GameObject playerPrefab)
     {
-        GameObject player = Object.Instantiate(
+        GameObject player = UnityEngine.Object.Instantiate(
                                 playerPrefab, 
                                 Vector3.zero, 
                                 Quaternion.identity) as GameObject;
@@ -17,13 +21,13 @@ public class PlayerGenerator : MonoBehaviour
 
     public static GameObject GeneratePlayer(PlayerDTO playerDTO)
     {
-        GameObject deePrefab = Resources.Load<GameObject>("Prefabs/Players/player_dee");
-
-        GameObject player = Object.Instantiate(
-                                deePrefab,
+        GameObject player = UnityEngine.Object.Instantiate(
+                                PlayerGenerator.deePrefab,
                                 new Vector3(playerDTO.location.x, 0, playerDTO.location.y),
                                 Quaternion.identity) as GameObject;
+        
         player.transform.SetParent(GameObject.Find("Players").transform, false);
+        player.transform.Find("Dee").GetComponent<Renderer>().material = PlayerGenerator.materials[UnityEngine.Random.Range(0, materials.Length - 1)];
 
         return player;            
     }
--- UnityProject/Assets/Scripts/Players/PlayerManager.cs
+++ UnityProject/Assets/Scripts/Players/PlayerManager.cs
@@ -66,7 +66,7 @@ namespace Players
 
             activePlayers.Add(PlayerId(playerDTO.id), player);
 
-            //Random Texture 
+            //Random Texture For Each Player - CHANGE when functionality added.
             Material material = (Material)Resources.LoadAll("Materials/Players", typeof(Material))[UnityEngine.Random.Range(0, 4)];
             player.transform.Find("Dee").GetComponent<Renderer>().material = material;
 
