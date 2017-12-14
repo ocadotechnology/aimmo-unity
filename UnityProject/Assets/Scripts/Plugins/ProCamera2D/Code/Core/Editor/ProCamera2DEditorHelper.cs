b43f89675f05e9979ab06a9167188965c4559d47
Niket Shah <masterniket@gmail.com>
2017-12-14 11:42:17 +0000

    Camera controls (#141)
    * Camera Controls Added
    
    * Remove custom Camera scripts
    
    * Add ProCamera2D plugin to project
    
    * don’t diff plugins
    
    * Ignore ProCamera2D gizmo
    
    * Attempt to correct gitattributes
    
    * Attempt to correct gitattributes
    
    * Attempt again
    
    * Delete unused parts of the ProCamera2D plugin

---
+++ UnityProject/Assets/Scripts/Plugins/ProCamera2D/Code/Core/Editor/ProCamera2DEditorHelper.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Com.LuisPedroFonseca.ProCamera2D
+{
+    public class ProCamera2DEditorHelper : Editor
+    {
+        public static void AssignProCamera2D(BasePC2D target)
+        {
+            if (target.ProCamera2D == null && Camera.main != null)
+                target.ProCamera2D = Camera.main.GetComponent<ProCamera2D>();
+
+            if (target.ProCamera2D == null)
+                target.ProCamera2D = FindObjectOfType<ProCamera2D>();
+            
+            if (target.ProCamera2D == null)
+                Debug.LogError("No ProCamera2D component found. Please add one to your camera.");
+        }
+    }
+}
---
+++ UnityProject/Assets/Scripts/Plugins/ProCamera2D/Code/Extensions/ProCamera2DGeometryBoundaries.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace Com.LuisPedroFonseca.ProCamera2D
+{
+    #if UNITY_5_3_OR_NEWER
+    [HelpURLAttribute("http://www.procamera2d.com/user-guide/extension-geometry-boundaries/")]
+    #endif
+    public class ProCamera2DGeometryBoundaries : BasePC2D, IPositionDeltaChanger
+    {
+        public static string ExtensionName = "Geometry Boundaries";
+
+        [Tooltip("The layer that contains the (3d) colliders that define the boundaries for the camera")]
+        public LayerMask BoundariesLayerMask;
+
+        MoveInColliderBoundaries _cameraMoveInColliderBoundaries;
+
+        override protected void Awake()
+        {
+            base.Awake();
+
+            _cameraMoveInColliderBoundaries = new MoveInColliderBoundaries(ProCamera2D);
+            _cameraMoveInColliderBoundaries.CameraTransform = ProCamera2D.transform;
+            _cameraMoveInColliderBoundaries.CameraCollisionMask = BoundariesLayerMask;
+
+            ProCamera2D.AddPositionDeltaChanger(this);
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+
+            if(ProCamera2D)
+                ProCamera2D.RemovePositionDeltaChanger(this);
+        }
+
+        #region IPositionDeltaChanger implementation
+
+        public Vector3 AdjustDelta(float deltaTime, Vector3 originalDelta)
+        {
+            if (!enabled)
+                return originalDelta;
+            
+            _cameraMoveInColliderBoundaries.CameraSize = ProCamera2D.ScreenSizeInWorldCoordinates;
+
+            // Apply movement considering the collider boundaries
+            return _cameraMoveInColliderBoundaries.Move(originalDelta);
+        }
+
+        public int PDCOrder { get { return _pdcOrder; } set { _pdcOrder = value; } }
+        int _pdcOrder = 3000;
+
+        #endregion
+    }
+}
