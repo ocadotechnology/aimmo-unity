fc9676ff62a73737a59ae69fe77cb6eab6731985
Niket Shah <masterniket@gmail.com>
2017-12-14 11:22:40 +0000

    Add ProCamera2D plugin to project

---
+++ UnityProject/Assets/Scripts/Plugins/ProCamera2D/Code/Extensions/Editor/ProCamera2DGeometryBoundariesEditor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Com.LuisPedroFonseca.ProCamera2D
+{
+    [CustomEditor(typeof(ProCamera2DGeometryBoundaries))]
+    public class ProCamera2DGeometryBoundariesEditor : Editor
+    {
+        void OnEnable()
+        {
+            if (target == null)
+                return;
+            
+            ProCamera2DEditorHelper.AssignProCamera2D(target as BasePC2D);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            if (target == null)
+                return;
+            
+            var proCamera2DGeometryBoundaries = (ProCamera2DGeometryBoundaries)target;
+
+            if (proCamera2DGeometryBoundaries.ProCamera2D == null)
+            {
+                EditorGUILayout.HelpBox("ProCamera2D is not set.", MessageType.Error, true);
+                return;
+            }
+
+            DrawDefaultInspector();
+
+            if (proCamera2DGeometryBoundaries.ProCamera2D.CenterTargetOnStart)
+                EditorGUILayout.HelpBox("Centering on target at start while using geometry boundaries might cause the camera to get stuck.", MessageType.Warning, true);
+            
+        }
+    }
+}
---
+++ UnityProject/Assets/Scripts/Plugins/ProCamera2D/Examples/TopDownShooter/Scripts/GameOver.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Com.LuisPedroFonseca.ProCamera2D.TopDownShooter
+{
+    public class GameOver : MonoBehaviour
+    {
+        public Canvas GameOverScreen;
+
+        void Awake()
+        {
+            GameOverScreen.gameObject.SetActive(false);
+        }
+
+        public void ShowScreen()
+        {
+            GameOverScreen.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        public void PlayAgain()
+        {
+            Time.timeScale = 1;
+
+            #if UNITY_5_3_OR_NEWER
+            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+            #else
+            Application.LoadLevel(Application.loadedLevel);
+            #endif
+        }
+    }
+}
