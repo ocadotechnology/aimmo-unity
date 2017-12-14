2b6105bb7303b929e9bcff86a6c1e977858df6cd
Maria Mafalda Fernando <maria.fernando@ocado.com>
2017-12-14 10:26:37 +0000

    Camera Controls Added

---
+++ UnityProject/Assets/Scripts/Zoom.cs
@@ -0,0 +1,26 @@
+﻿ using UnityEngine;
+ using System.Collections;
+ 
+ public class Zoom : MonoBehaviour {
+     
+     public float zoomSpeed = 1;
+     public float targetOrtho;
+     public float smoothSpeed = 2.0f;
+     public float minOrtho = 1.0f;
+     public float maxOrtho = 20.0f;
+     
+     void Start() {
+         targetOrtho = Camera.main.orthographicSize;
+     }
+     
+     void Update () {
+         
+         float scroll = Input.GetAxis ("Mouse ScrollWheel");
+         if (scroll != 0.0f) {
+             targetOrtho -= scroll * zoomSpeed;
+             targetOrtho = Mathf.Clamp (targetOrtho, minOrtho, maxOrtho);
+         }
+         
+         Camera.main.orthographicSize = Mathf.MoveTowards (Camera.main.orthographicSize, targetOrtho, smoothSpeed * Time.deltaTime);
+     }
+ }
