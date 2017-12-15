8c84d66bab52a282bce711e9fe526ce05d2a3239
Olaf Szmidt <szmidtolaf@gmail.com>
2017-12-15 11:56:56 +0000

    Hashing for unique skins (#140)
    * Use a simple modulus hashing function to generate unique skins
    
    * Write a test for the new hashing functionality
    
    * Change visibility declarations and delete unused imports of ns's

---
+++ UnityProject/Assets/Editor/Tests/PlayerGeneratorTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using NUnit.Framework;
+
+namespace AIMMOUnityTest
+{
+    [TestFixture]
+    internal class PlayerGeneratorTest
+    {
+        [Test]
+        public void TestPlayerGeneratorMapIDToSkin()
+        {
+            // ID 1 to Material 1
+            Material expectedSkin = PlayerGenerator.materials[1];
+            int playerID = 1;
+
+            Material receivedSkin = PlayerGenerator.mapIDToSkin(playerID);
+
+            Assert.AreSame(expectedSkin, receivedSkin);
+
+            // ID 3 to Material 3
+            expectedSkin = PlayerGenerator.materials[3];
+            playerID = 3;
+
+            receivedSkin = PlayerGenerator.mapIDToSkin(playerID);
+
+            Assert.AreSame(expectedSkin, receivedSkin);
+
+            // ID materials.Length to Material 0
+            expectedSkin = PlayerGenerator.materials[0];
+            playerID = PlayerGenerator.numberOfSkins;
+
+            receivedSkin = PlayerGenerator.mapIDToSkin(playerID);
+
+            Assert.AreSame(expectedSkin, receivedSkin);
+
+            // ID materials.Length + 1 to Material 1
+            expectedSkin = PlayerGenerator.materials[1];
+            playerID = PlayerGenerator.numberOfSkins + 1;
+
+            receivedSkin = PlayerGenerator.mapIDToSkin(playerID);
+
+            Assert.AreSame(expectedSkin, receivedSkin);
+        }
+    }
+}
--- UnityProject/Assets/Scripts/Generators/PlayerGenerator.cs
+++ UnityProject/Assets/Scripts/Generators/PlayerGenerator.cs
@@ -3,8 +3,9 @@ using Players;
 
 public class PlayerGenerator : MonoBehaviour
 {
-    static Material[] materials = Resources.LoadAll<Material>("Materials/Players");
+    public static Material[] materials = Resources.LoadAll<Material>("Materials/Players");
     static GameObject deePrefab = Resources.Load<GameObject>("Prefabs/Players/player_dee");
+    public static int numberOfSkins = materials.Length;
 
     public static GameObject GeneratePlayer(GameObject playerPrefab)
     {
@@ -25,10 +26,14 @@ public class PlayerGenerator : MonoBehaviour
                                 Quaternion.identity) as GameObject;
         
         player.transform.SetParent(GameObject.Find("Players").transform, false);
-        player.transform.Find("Dee").GetComponent<Renderer>().material = PlayerGenerator.materials[Random.Range(0, materials.Length - 1)];
+        player.transform.Find("Dee").GetComponent<Renderer>().material = PlayerGenerator.mapIDToSkin(playerDTO.id);
 
         return player;            
     }
 
+    public static Material mapIDToSkin(int playerID)
+    {
+        return PlayerGenerator.materials[playerID % numberOfSkins];
+    }
 }
 
