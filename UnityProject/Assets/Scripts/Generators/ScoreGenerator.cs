811e178fdc581255334ee3d8a19c533aac8815a9
Niket Shah <masterniket@gmail.com>
2018-01-15 15:47:54 +0000

    Make ScoreGenerator tests independant of scene opened

--- UnityProject/Assets/Scripts/Generators/ScoreGenerator.cs
+++ UnityProject/Assets/Scripts/Generators/ScoreGenerator.cs
@@ -3,18 +3,18 @@ using MapFeatures.ScoreLocations;
 
 public class ScoreGenerator
 {
-    public static GameObject GenerateScore(GameObject scorePrefab)
+    public static GameObject GenerateScore(GameObject scorePrefab, Transform parent)
     {
         GameObject score = Object.Instantiate(
-            scorePrefab, 
-            Vector3.zero, 
+            scorePrefab,
+            Vector3.zero,
             Quaternion.identity) as GameObject;
-        score.transform.SetParent(GameObject.Find("Scores").transform, false);
+        score.transform.SetParent(parent, false);
 
         return score;
     }
 
-    public static GameObject GenerateScore(ScoreLocationDTO scoreDTO)
+    public static GameObject GenerateScore(ScoreLocationDTO scoreDTO, Transform parent)
     {
         // TODO: pass type from back-end. Hardcoded to bluedisk for now.
         string resource = "Prefabs/Scores/score_" + "bluedisk";
@@ -24,11 +24,11 @@ public class ScoreGenerator
             scorePrefab,
             new Vector3(scoreDTO.location.x, 0, scoreDTO.location.y),
             Quaternion.identity) as GameObject;
-        score.transform.SetParent(GameObject.Find("Scores").transform, false);
+        score.transform.SetParent(parent, false);
 
         // TODO: make name dynamic to type passed too.
         score.name = "score_" + "bluedisk" + "_" + scoreDTO.location.x + "_" + scoreDTO.location.y;
 
-        return score;            
+        return score;
     }
 }
