594ea7cd98b2b383dc4d686ba15274b839b1fdff
Olaf Szmidt <szmidtolaf@gmail.com>
2018-03-14 16:15:40 +0000

    Add test for this marker functionality

---
+++ UnityProject/Assets/Editor/Tests/PlayerManagerTest.cs
@@ -0,0 +1,59 @@
+using System;
+using UnityEngine;
+using NUnit.Framework;
+using Players;
+
+namespace AIMMOUnityTest
+{
+    [TestFixture]
+    internal class PlayerManagerTest
+    {
+        GameObject parentStub;
+        GameObject playersFolder;
+        PlayerManager playerManager;
+        PlayerDTO playerDTO;
+        PlayerDTO player2DTO;
+
+        [SetUp]
+        public void SetUpPlayerManager()
+        {
+            parentStub = new GameObject("ParentStub");
+            playersFolder = new GameObject("PlayersFolder");
+            playerManager = parentStub.AddComponent<PlayerManager>();
+            playerManager.parentPlayerFolder = playersFolder;
+        }
+
+        [SetUp]
+        public void SetUpPlayerDTO()
+        {
+            playerDTO = new PlayerDTO();
+            playerDTO.id = 1;
+
+            player2DTO = new PlayerDTO();
+            player2DTO.id = 2;
+        }
+
+        [Test]
+        public void TestCorrectPlayerAddedWithMarker()
+        {
+            // Set (mock) desired ID to 2
+            playerManager.avatarID = 2;
+
+            playerManager.CreatePlayer(playerDTO);
+
+            GameObject player = playersFolder.transform.Find("player1").gameObject;
+            Assert.IsNotNull(player);
+            Assert.AreEqual(player.tag, "Avatar");
+
+            playerManager.CreatePlayer(player2DTO);
+
+            GameObject player2 = playersFolder.transform.Find("player2").gameObject;
+            Assert.IsNotNull(player2);
+            Assert.AreEqual(player2.tag, "Avatar");
+
+
+            GameObject marker = player2.transform.Find("player_arrow(Clone)").gameObject;
+            Assert.IsNotNull(marker);
+        }
+    }
+}
