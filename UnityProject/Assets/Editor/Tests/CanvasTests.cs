24bb631074d7caaf0fac69c2cb5c5750220af794
Olaf Szmidt <szmidtolaf@gmail.com>
2018-03-20 15:12:16 +0000

    Add Canvas Tests

---
+++ UnityProject/Assets/Editor/Tests/CanvasTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using UnityEngine;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace AIMMOUnityTest
+{
+    [TestFixture]
+    internal class CanvasTests
+    {
+        private void SetUpMainScene()
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/Main.unity", OpenSceneMode.Single);
+        }
+
+        [Test]
+        public void TestMainSceneHasCompass()
+        {
+            SetUpMainScene();
+
+            // Main game object.
+            GameObject compass = GameObject.Find("Compass");
+
+            // All relevant components.
+            RectTransform rectTransform = compass.GetComponent(typeof(RectTransform)) as RectTransform;
+            CanvasRenderer canvasRenderer = compass.GetComponent(typeof(CanvasRenderer)) as CanvasRenderer;
+            Image image = compass.GetComponent(typeof(Image)) as Image;
+
+            Assert.NotNull(compass);
+            Assert.NotNull(rectTransform);
+            Assert.NotNull(image);
+        }
+
+        [Test]
+        public void TestMainSceneCompassHasCorrectPositionRelativeToAnchor()
+        {
+            SetUpMainScene();
+            GameObject compass = GameObject.Find("Compass");
+
+            RectTransform rectTransform = compass.GetComponent(typeof(RectTransform)) as RectTransform;
+
+            Vector3 expectedAnchoredPos = new Vector3(64.3114f, 60.00f, -1.00f);
+            Vector3 realAnchoredPos = rectTransform.anchoredPosition3D;
+
+            Assert.True(Mathf.Approximately(expectedAnchoredPos.x, realAnchoredPos.x));
+            Assert.True(Mathf.Approximately(expectedAnchoredPos.y, realAnchoredPos.y));
+            Assert.True(Mathf.Approximately(expectedAnchoredPos.z, realAnchoredPos.z));
+        }
+
+        [Test]
+        public void TestMainSceneHasCanvasRendererAttatched()
+        {
+            SetUpMainScene();
+            GameObject compass = GameObject.Find("Compass");
+
+            CanvasRenderer canvasRenderer = compass.GetComponent(typeof(CanvasRenderer)) as CanvasRenderer;
+
+            Assert.NotNull(canvasRenderer);
+        }
+
+        [Test]
+        public void TestMainSceneCompassHasImageScriptAttatchedCorrectly()
+        {
+            SetUpMainScene();
+            GameObject compass = GameObject.Find("Compass");
+            Image image = compass.GetComponent(typeof(Image)) as Image;
+            String expectedTextureName = "compass2";
+            Texture expectedTexture = (Texture) Resources.Load("Images/compass2", typeof(Texture));
+
+            Assert.AreEqual(expectedTextureName, image.mainTexture.name);
+            Assert.AreEqual(expectedTexture, image.mainTexture);
+        }
+    }
+}     
