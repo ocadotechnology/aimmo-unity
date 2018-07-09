807d6efac668f6f17c33442c1e12ba24992a8201
Niket Shah <masterniket@gmail.com>
2018-07-09 14:14:07 +0100

    Update Game exporter for aimmo pypi rename

--- UnityProject/Assets/Editor/Game Exporter/GameExporter.cs
+++ UnityProject/Assets/Editor/Game Exporter/GameExporter.cs
@@ -3,12 +3,12 @@ using UnityEditor;
 
 public class GameExporter
 {
-	[MenuItem("Build Game/Build WebGL Game into aimmo")]
-	static void BuildGame()
-	{
+    [MenuItem("Build Game/Build WebGL Game into aimmo")]
+    static void BuildGame()
+    {
         string path = EditorUtility.SaveFolderPanel("Select Location of `aimmo` Repository", "", "");
         string[] levels = new string[] { "Assets/Scenes/Main.unity" };
 
-        BuildPipeline.BuildPlayer(levels, path + "/players/static/unity", BuildTarget.WebGL, BuildOptions.None);
-	}
+        BuildPipeline.BuildPlayer(levels, path + "/aimmo/static/unity", BuildTarget.WebGL, BuildOptions.None);
+    }
 }
