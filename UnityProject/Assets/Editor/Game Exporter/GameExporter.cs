28cafe2fc00558f268ca29b6ae77a02e3475b6cd
Jacob Moss <parallaxparadox@gmail.com>
2018-07-26 15:43:21 +0100

    check if folder exists

--- UnityProject/Assets/Editor/Game Exporter/GameExporter.cs
+++ UnityProject/Assets/Editor/Game Exporter/GameExporter.cs
@@ -6,16 +6,27 @@ public class GameExporter
     [MenuItem("Build Game/Build WebGL Game into aimmo")]
     static void BuildGame()
     {
+        // Adding the tilde at the end of the plugin, and reimporting results in Unity ignoring the library
         Directory.Move("Assets/Scripts/Plugins/UnitySocketIO", "Assets/Scripts/Plugins/UnitySocketIO~");
         AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
-        string path = EditorUtility.SaveFolderPanel("Select Location of `aimmo` Repository", "", "");
 
-        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-        buildPlayerOptions.scenes = new string[] { "Assets/Scenes/Main.unity" };
-        buildPlayerOptions.locationPathName = path + "/aimmo/static/unity";
-        buildPlayerOptions.target = BuildTarget.WebGL;
-        buildPlayerOptions.options = BuildOptions.None;
-        var report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        string path = EditorUtility.SaveFolderPanel("Select Location of `aimmo` Repository", "", "");
+        path = path + "/aimmo/static/unity";
+        if (!Directory.Exists(path))
+        {
+            EditorUtility.DisplayDialog("No Unity folder", "I cannot find a Unity folder at: " + path, "Cancel");
+        }
+        else 
+        {
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+            {
+                scenes = new string[] { "Assets/Scenes/Main.unity" },
+                locationPathName = path,
+                target = BuildTarget.WebGL,
+                options = BuildOptions.None
+            };
+            BuildPipeline.BuildPlayer(buildPlayerOptions);
+        }
 
         Directory.Move("Assets/Scripts/Plugins/UnitySocketIO~", "Assets/Scripts/Plugins/UnitySocketIO");
         AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
