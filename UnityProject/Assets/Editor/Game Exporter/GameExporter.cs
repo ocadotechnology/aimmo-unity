560e48c260f3791515aade8f8a91a0a90d3c4532
Jacob Moss <parallaxparadox@gmail.com>
2018-07-30 16:32:08 +0100

    Don't include socket package in WebGL build (#227)
    * add tilde
    
    * check if folder exists
    
    * add dev build option
    
    * add helpful comment
    
    * wrap import in macro

--- UnityProject/Assets/Editor/Game Exporter/GameExporter.cs
+++ UnityProject/Assets/Editor/Game Exporter/GameExporter.cs
@@ -1,14 +1,46 @@
-using UnityEngine;
 using UnityEditor;
+using System.IO;
 
 public class GameExporter
 {
     [MenuItem("Build Game/Build WebGL Game into aimmo")]
-    static void BuildGame()
+    static void BuildProdGame()
     {
-        string path = EditorUtility.SaveFolderPanel("Select Location of `aimmo` Repository", "", "");
-        string[] levels = new string[] { "Assets/Scenes/Main.unity" };
-
-        BuildPipeline.BuildPlayer(levels, path + "/aimmo/static/unity", BuildTarget.WebGL, BuildOptions.None);
+        BuildGame(false);
     }
+    
+    [MenuItem("Build Game/Build Development WebGL Game into aimmo")]
+    static void BuildDevGame()
+    {
+        BuildGame(true);
+    }
+
+    static void BuildGame(bool development)
+    {
+        // Adding the tilde at the end of the plugin, and reimporting results in Unity ignoring the library
+        Directory.Move("Assets/Scripts/Plugins/UnitySocketIO", "Assets/Scripts/Plugins/UnitySocketIO~");
+        AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
+
+        string path = EditorUtility.SaveFolderPanel("Select Location of `aimmo` Repository", "", "");
+        path = path + "/aimmo/static/unity";
+        if (!Directory.Exists(path))
+        {
+            EditorUtility.DisplayDialog("No Unity folder", "I cannot find a Unity folder at: " + path, "Cancel");
+        }
+        else
+        {
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+            {
+                scenes = new string[] { "Assets/Scenes/Main.unity" },
+                locationPathName = path,
+                target = BuildTarget.WebGL,
+                options = development ? BuildOptions.Development : BuildOptions.None
+            };
+            BuildPipeline.BuildPlayer(buildPlayerOptions);
+        }
+
+        Directory.Move("Assets/Scripts/Plugins/UnitySocketIO~", "Assets/Scripts/Plugins/UnitySocketIO");
+        AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate); // Stop ignoring the library
+    }
+
 }
